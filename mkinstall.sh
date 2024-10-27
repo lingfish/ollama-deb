63ee511207dbb951e5ed075f90d2db3e978381ff
Jason Lingohr <lingfish@users.noreply.github.com>
2024-10-27 14:09:54 +1100

    Create main.yml

---
+++ mkinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -f debian/install
+
+do_bin () {
+  for file in tmp/bin/*; do
+    # Skip directories (only process files)
+    if [ -f "$file" ]; then
+      f=$(basename -- "$file")
+      echo "tmp/bin/$f usr/bin/" >> debian/install
+    fi
+  done
+}
+
+do_libs () {
+  for file in tmp/lib/ollama/*; do
+    # Skip directories (only process files)
+    if [ -f "$file" ]; then
+      f=$(basename -- "$file")
+      echo "tmp/lib/ollama/$f usr/lib/ollama/" >> debian/install
+    fi
+  done
+}
+
+do_bin
+do_libs
